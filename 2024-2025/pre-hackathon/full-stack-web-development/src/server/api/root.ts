c13727ffe418536152c779f363be18406cd58df7
Caleb Rivera <calebrossr@gmail.com>
2024-08-21 16:52:36 -0400

    Clean up boiler plate

--- 2024-2025/pre-hackathon/full-stack-web-development/src/server/api/root.ts
+++ 2024-2025/pre-hackathon/full-stack-web-development/src/server/api/root.ts
@@ -1,5 +1,9 @@
-import { postRouter } from "~/server/api/routers/post";
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { z } from "zod";
+import {
+  createCallerFactory,
+  createTRPCRouter,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +11,13 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
 });
 
 // export type definition of API
