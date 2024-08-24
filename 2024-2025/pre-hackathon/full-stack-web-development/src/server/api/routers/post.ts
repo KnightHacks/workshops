0019379d5f694ff1c398ef6756044673edb7862b
Caleb Rivera <calebrossr@gmail.com>
2024-08-24 19:26:57 -0400

    Add delete, update, and read all procedures to post router

--- 2024-2025/pre-hackathon/full-stack-web-development/src/server/api/routers/post.ts
+++ 2024-2025/pre-hackathon/full-stack-web-development/src/server/api/routers/post.ts
@@ -1,6 +1,7 @@
 import { posts } from "~/server/db/schema";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { z } from "zod";
+import { desc, eq } from "drizzle-orm";
 
 export const postRouter = createTRPCRouter({
   create: protectedProcedure
@@ -11,4 +12,43 @@ export const postRouter = createTRPCRouter({
         text: input,
       });
     }),
+  delete: protectedProcedure
+    .input(z.number())
+    .mutation(async ({ ctx, input }) => {
+      const postToDelete = await ctx.db.query.posts.findFirst({
+        where: eq(posts.id, input),
+      });
+
+      if (!postToDelete || postToDelete.userId !== ctx.session.user.id) {
+        return;
+      }
+
+      await ctx.db.delete(posts).where(eq(posts.id, input));
+    }),
+  update: protectedProcedure
+    .input(z.object({ id: z.number(), text: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const postToUpdate = await ctx.db.query.posts.findFirst({
+        where: eq(posts.id, input.id),
+      });
+
+      if (!postToUpdate || postToUpdate.userId !== ctx.session.user.id) {
+        return;
+      }
+
+      await ctx.db
+        .update(posts)
+        .set({
+          text: input.text,
+        })
+        .where(eq(posts.id, input.id));
+    }),
+  all: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.query.posts.findMany({
+      orderBy: desc(posts.createdAt),
+      with: {
+        user: true,
+      },
+    });
+  }),
 });
