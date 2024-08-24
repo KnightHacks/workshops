62be673a6a6c9108bb1a7b9e3a252025077e7a0c
Caleb Rivera <calebrossr@gmail.com>
2024-08-23 21:26:48 -0400

    Add auth

--- 2024-2025/pre-hackathon/full-stack-web-development/src/app/page.tsx
+++ 2024-2025/pre-hackathon/full-stack-web-development/src/app/page.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   const hello = await api.hello({ text: "from tRPC" });
+  const session = await getServerAuthSession();
 
   return (
     <HydrateClient>
       <main>{hello.greeting}</main>
+      <Link passHref href={session ? "/api/auth/signout" : "/api/auth/signin"}>
+        <Button>{session ? "Sign out" : "Sign in"}</Button>
+      </Link>
     </HydrateClient>
   );
 }
